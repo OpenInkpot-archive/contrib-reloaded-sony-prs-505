5923fa11a7caddd441913a6dfa43f2ed56d1347f
Yauhen Kharuzhy <jekhor@gmail.com>
2011-01-23 16:07:56 +0200

    Fix serial port name in default kernel cmdline
    Signed-off-by: Yauhen Kharuzhy <jekhor@gmail.com>

--- board.h
+++ board.h
@@ -12,7 +12,7 @@
 
 #define DEFAULT_KERNEL_PATH 	"/mnt/zImage"
 #define DEFAULT_INITRD_PATH 	""
-#define DEFAULT_CMDLINE		"rw console=ttySMX1,115200n8 ip=192.168.111.1 root=/dev/nfs nfsroot=192.168.111.2:/srv/nfs/openinkpot-root"
+#define DEFAULT_CMDLINE		"rw console=ttymxc1,115200n8 ip=192.168.111.1 root=/dev/nfs nfsroot=192.168.111.2:/srv/nfs/openinkpot-root"
 #define DEFAULT_MACHTYPE	2007
 
 #endif
